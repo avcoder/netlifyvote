91ffc7fae1d1072215ef4d27af744d204db9b3fb
avcoder <code.rom7.6@gmail.com>
2020-10-12 09:44:20 -0400

    changed from hello world to actual voting code

--- functions/hello.js
+++ functions/hello.js
@@ -1,7 +1,24 @@
+let votesArray = [0, 0, 0, 0];
+
 exports.handler = async (event) => {
-  const subject = event.queryStringParameters.name || "world";
+  const vote = event.queryStringParameters.vote;
+
+  if (vote === "a") {
+    votesArray[0] += 1;
+  } else if (vote === "b") {
+    votesArray[1] += 1;
+  } else if (vote === "c") {
+    votesArray[2] += 1;
+  } else if (vote === "d") {
+    votesArray[3] += 1;
+  } else if (vote === "r") {
+    votesArray = [0, 0, 0, 0];
+  }
+
+  console.log(JSON.stringify(votesArray));
+
   return {
     statusCode: 200,
-    body: `Hello ${subject}`,
+    body: JSON.stringify(votesArray),
   };
 };
